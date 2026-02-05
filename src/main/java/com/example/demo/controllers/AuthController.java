e0e6442a2682c3bafb61f4d62719d10f2afda6ac
FadyWael10 <169147111+FadyWael10@users.noreply.github.com>
2026-02-05 15:22:32 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.LoginRequest;
+import com.example.demo.dto.SignupRequest;
+import com.example.demo.service.AuthService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody SignupRequest request) {
+        return authService.signup(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
